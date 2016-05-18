8e6b54eccf949e0d007ece2e1c63da2ced31c570
ritliu <ritliu@stubhub.com>
2016-05-18 09:08:57 -0700

    missing check in test case

--- bigdime-runtimeinfo-management/src/test/java/io/bigdime/runtimeinfo/RuntimeInfoRepositoryServiceTest.java
+++ bigdime-runtimeinfo-management/src/test/java/io/bigdime/runtimeinfo/RuntimeInfoRepositoryServiceTest.java
@@ -3,7 +3,9 @@
  */
 package io.bigdime.runtimeinfo;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -18,6 +20,7 @@ import org.testng.annotations.Test;
 
 import io.bigdime.runtimeinfo.impl.RuntimeInfoRepositoryService;
 import io.bigdime.runtimeinfo.DTO.RuntimeInfoDTO;
+import io.bigdime.runtimeinfo.DTO.RuntimePropertyDTO;
 import io.bigdime.runtimeinfo.repositories.RuntimeInfoRepository;
 
 public class RuntimeInfoRepositoryServiceTest {
@@ -29,6 +32,9 @@ public class RuntimeInfoRepositoryServiceTest {
 
 	@Mock
 	RuntimeInfoRepository mockRuntimeInfoRepository;
+	
+	@Mock
+	RuntimePropertyDTO mockRuntimePropertyDTO;
 	
 	
 
@@ -62,10 +68,14 @@ public class RuntimeInfoRepositoryServiceTest {
 						.findByAdaptorNameAndEntityNameAndInputDescriptor(
 								mockRuntimeInfo.getAdaptorName(), mockRuntimeInfo.getEntityName(), mockRuntimeInfo.getInputDescriptor()))
 				.thenReturn(Mockito.mock(RuntimeInfoDTO.class));
+		Set<RuntimePropertyDTO> runtimePropertyDTO = new HashSet<RuntimePropertyDTO>();
+		runtimePropertyDTO.add(mockRuntimePropertyDTO);
+		when(mockRuntimeInfo.getRuntimeProperties()).thenReturn(runtimePropertyDTO);
 		
 		Assert.assertTrue(runtimeInfoRepositoryServce.create(mockRuntimeInfo));
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testGetAdapterEntries() {
 		
